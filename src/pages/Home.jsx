24a85fe76bf8c09340ee582a2de6b3647ecbf939
Shardul Pathak <48183855+shapaaa@users.noreply.github.com>
2021-12-14 11:03:06 +0530

    Show emoji on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Github from '../assets/icons/github'
 import Twitter from '../assets/icons/twitter'
 import Linkedin from '../assets/icons/linkedin'
 import Dev from '../assets/icons/dev'
-
+import Emoji from '../assets/icons/Emoji.png';
 const Container = styled.section`
 display: flex;
 flex-direction: column;
@@ -22,8 +22,8 @@ const fadeinup = keyframes`
 `
 const Image = styled.img`
 margin:20px 0;
-width: 300px;
-height:300px;
+width: 350px;
+height:350px;
 transform: translateY(200px);
 opacity: 0;
 animation: ${fadeinup} 1.1s .02s 1 ease-in-out forwards;
@@ -32,7 +32,11 @@ const Info = styled.p`
 font-size: 20px;
 margin-bottom: 0;
 text-align:center;
-line-height:1.8em;
+line-height:1.5em;
+@media only screen  and (max-width:530px){
+  line-height: 1.2;
+  justify-self: flex-start;
+}
 transform: translateY(150px);
 opacity: 0;
 animation: ${fadeinup} 1s .12s 1 ease-in-out forwards;
@@ -43,18 +47,37 @@ opacity: 0;
 transform: translateY(150px);
 animation: ${fadeinup} 1s .14s 1 ease-in-out forwards;
 `
+const Wrapper = styled.div`
+display: flex;
+@media only screen  and (max-width:514px){
+  flex-direction: column;
+  margin-top: 10px;
+}
+`
+const SocialMediaContainer = styled.div`
+margin: 30px 0;
+align-items: center;
+display: flex;
+flex-direction: column;
+`
 const Home = () => {
     return (
         <Container>
-            <Image src='https://www.dhilipkmr.dev/static/profilePic-162001e6f25969e3061354534123fea9.png' />
+            <Image src={Emoji} />
             <Info>Software Engineer @rtCamp</Info>
-            <Info>lifelong learner | amateur blogger | empath | occasional traveller</Info>
-            <SocialInfo>
-                <a target='_blank' rel="noreferrer" href='https://github.com/shapaaa'> <Github /> </a>
-                <a target='_blank' rel="noreferrer" href='https://twitter.com/Shardul52754488'> <Twitter /></a>
-                <a target='_blank' rel="noreferrer" href='https://www.linkedin.com/in/shardul-pathak-718694176/'><Linkedin /> </a>
-                <a target='_blank' rel="noreferrer" href='https://dev.to/shapaaa'> <Dev /></a>
-            </SocialInfo>
+            <Wrapper>
+                <Info>Programmer • Developer • Learner </Info>
+                <Info>&nbsp;• Amateur Blogger</Info>
+            </Wrapper>
+            <SocialMediaContainer>
+                <Info> Also you can find me on </Info>
+                <SocialInfo>
+                    <a target='_blank' rel="noreferrer" href='https://github.com/shapaaa'> <Github /> </a>
+                    <a target='_blank' rel="noreferrer" href='https://twitter.com/Shardul52754488'> <Twitter /></a>
+                    <a target='_blank' rel="noreferrer" href='https://www.linkedin.com/in/shardul-pathak-718694176/'><Linkedin /> </a>
+                    <a target='_blank' rel="noreferrer" href='https://dev.to/shapaaa'> <Dev /></a>
+                </SocialInfo>
+            </SocialMediaContainer>
         </Container>
     )
 }
