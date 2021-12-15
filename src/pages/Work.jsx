f6e9616f126b86af47a284aee1330bd1297c9a97
Shardul Pathak <48183855+shapaaa@users.noreply.github.com>
2021-12-15 14:20:01 +0530

    Add redirects

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,6 +1,6 @@
 import styled, { css, keyframes } from "styled-components";
 import Github from "../assets/icons/github";
-import Project, { LinkContent  } from "../components/Project";
+import Project, { LinkContent } from "../components/Project";
 import portfolio from '../assets/videos/portfolio.webm'
 import wordcamp from '../assets/videos/wordcamp.webm'
 import weather from '../assets/videos/weather.webm'
@@ -18,7 +18,7 @@ gap:40px;
   width: 80%;
   margin: 0 auto;
   `
-  const fadeinup = keyframes`
+const fadeinup = keyframes`
   100%{
     transform: translateY(0);
     opacity: 1;
@@ -33,14 +33,14 @@ text-align: center;
 transform: translateY(10%);
 opacity: 0;
 animation: ${fadeinup} .8s 0ms ease-out forwards;
-${({small})=> !small && css`
+${( { small } ) => !small && css`
     display: none;
 `}
 @media only screen and (min-width:530px)
 {
-    ${({small})=> small && css`
+    ${( { small } ) => small && css`
     display: none;`}
-    ${({small})=> !small && css`
+    ${( { small } ) => !small && css`
     display: block;`}
 }
 `
@@ -53,41 +53,47 @@ width:100%;
 `
 const projects = [
   {
-    name:'Portfolio Website',
-    video:portfolio,
-    about:'A Website to present my skills and projects I have done',
-    githubLink:'https://github.com/shapaaa/Portfolio-project'
+    id: 1,
+    name: 'Portfolio Website',
+    video: portfolio,
+    about: 'A Website to present my skills and projects I have done',
+    githubLink: 'https://github.com/shapaaa/Portfolio-project'
   },
   {
-    name:'Weather Application',
-    video:weather,
-    about:"A responsive web application to show weather data based on current location.Also user can get weather data about specific city by search",
-    siteLink:'https://weatherwebap.netlify.app/',
-    githubLink:'https://github.com/shapaaa/Weather-app'
+    id: 2,
+    name: 'Weather Application',
+    video: weather,
+    about: "A responsive web application to show weather data based on current location.Also user can get weather data about specific city by search",
+    siteLink: 'https://weatherwebap.netlify.app/',
+    githubLink: 'https://github.com/shapaaa/Weather-app'
   },
   {
-    name:'User Authentication',
-    about:'Built user authentication using JSON web token, React, web storage API, Graphql, Node.Used Postgres database for the application storage.',
-    githubLink:'https://github.com/shapaaa/grocery-app-server'
+    id: 3,
+    name: 'User Authentication',
+    about: 'Built user authentication using JSON web token, React, web storage API, Graphql, Node.Used Postgres database for the application storage.',
+    githubLink: 'https://github.com/shapaaa/grocery-app-server'
   },
   {
-    name:'WordCamp Events Application',
-    video:wordcamp,
-    about:'A web app to find wordcamp events happening all over the world using rest apis as data source',
-    siteLink:'https://wordcamp-central.netlify.app/',
-    githubLink:'https://github.com/shapaaa/WordCamp-Central-site'
+    id: 4,
+    name: 'WordCamp Events Application',
+    video: wordcamp,
+    about: 'A web app to find wordcamp events happening all over the world using rest apis as data source',
+    siteLink: 'https://wordcamp-central.netlify.app/',
+    githubLink: 'https://github.com/shapaaa/WordCamp-Central-site'
   },
   {
-    name:'Yelp Clone',
-    about:'A website where people can find reviews about hotels.Also user can add new hotels and reviews about them.Built using React, Node, Postgres db.Used node-pg as a connector.',
-    githubLink:'https://github.com/shapaaa/yelp-client'
+    id: 5,
+    name: 'Yelp Clone',
+    about: 'A website where people can find reviews about hotels.Also user can add new hotels and reviews about them.Built using React, Node, Postgres db.Used node-pg as a connector.',
+    githubLink: 'https://github.com/shapaaa/yelp-client'
   },
   {
-    name:'Adhelp.io',
-    video:adhelp,
-    about:'Responsive landing page with grid layout.Built using Html, Css , Chart.js library',
-    siteLink:'https://adhelp.netlify.app/',
-    githubLink:'https://github.com/shapaaa/Adhelp.io'
+    id: 6,
+    name: 'Adhelp.io',
+    video: adhelp,
+    about: 'Responsive landing page with grid layout.Built using Html, Css , Chart.js library',
+    siteLink: 'https://adhelp.netlify.app/',
+    githubLink: 'https://github.com/shapaaa/Adhelp.io'
   },
 ]
 const Link = styled.a`
@@ -131,17 +137,17 @@ transition: background-color .5s 0s ease;
 const Work = () => {
   return (
     <Container>
-        <Title small>Projects</Title>
-        <Title >Take a look at what I've been working on</Title>
-        <Projects>
-          {
-            projects.map((project)=>
-              <Project project={project}/>
-            )
-          }
-        </Projects>
-        <Link target="_blank" href="https://github.com/shapaaa?tab=repositories"><LinkContent>Find More &nbsp; <Github /></LinkContent></Link>
-    </Container>
+      <WorkTitle small>Projects</WorkTitle>
+      <WorkTitle>Take a look at what I've been working on!</WorkTitle>
+      <Projects>
+        {
+          projects.map( ( project ) =>
+            <Project key={project.id} project={project} />
+          )
+        }
+      </Projects>
+      <Link target="_blank" href="https://github.com/shapaaa?tab=repositories"><LinkContent>Find More &nbsp; <Github /></LinkContent></Link>
+    </Container >
   )
 }
 export default Work;
